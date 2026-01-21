a6d7408020027d7a7ae16a4553df071cc3bd5c2b
Patrickog1992 <patrickpneus2018@hotmail.com>
2026-01-21 11:04:04 -0300

    fix: Update video embed source
    Updates the video player's embed source URL. This is likely to fix an issue with the video not loading or to point to a new video.

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -18,9 +18,9 @@ const VideoPlayer: React.FC = () => {
     <div 
       dangerouslySetInnerHTML={{ 
         __html: `
-        <div id="ifr_69527fcf90b70171e382da44_wrapper" style="margin: 0 auto; width: 100%; max-width: 400px;"> 
-          <div style="position: relative; padding: 178.21782178217822% 0 0 0;" id="ifr_69527fcf90b70171e382da44_aspect"> 
-            <iframe frameborder="0" allowfullscreen src="about:blank" id="ifr_69527fcf90b70171e382da44" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" referrerpolicy="origin" onload=" this.onload=null, this.src='https://scripts.converteai.net/fd7cffcf-a128-4cf6-8573-7102145d7c17/players/69527fcf90b70171e382da44/v4/embed.html' +(location.search||'?') +'&vl=' +encodeURIComponent(location.href)"></iframe> 
+        <div id="ifr_6970db1ae1aa589ccc50298e_wrapper" style="margin: 0 auto; width: 100%; max-width: 400px;"> 
+          <div style="position: relative; padding: 178.21782178217822% 0 0 0;" id="ifr_6970db1ae1aa589ccc50298e_aspect"> 
+            <iframe frameborder="0" allowfullscreen src="about:blank" id="ifr_6970db1ae1aa589ccc50298e" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" referrerpolicy="origin" onload=" this.onload=null, this.src='https://scripts.converteai.net/66f7b845-c900-4cf3-b6f2-f848ebd65e7f/players/6970db1ae1aa589ccc50298e/v4/embed.html' +(location.search||'?') +'&vl=' +encodeURIComponent(location.href)"></iframe> 
           </div> 
         </div>`
       }} 
